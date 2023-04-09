30dd149fa02d4ddab07cd123b485e48882493532
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-04-09 02:30:43 +0200

    [ADD] missing circuit assets

--- src/media_generation/data.py
+++ src/media_generation/data.py
@@ -6,7 +6,7 @@ from .models import *
 bahrein = Circuit(id='bahrein', name='Bahreïn', city='Sakhir', lap_length=5.412, best_lap="1' 31'' 447")
 saudiarabia = Circuit(id='saudiarabia', name='Arabie Saoudite', city='Jeddah', lap_length=6.174, best_lap="1' 30'' 734")
 australia = Circuit(id='australia', name='Australie', city='Melbourne', lap_length=5.278, best_lap="N/A")
-imola = Circuit(id='imola', name='Emilie Romagne', city='Imola', lap_length=4.909, best_lap="1' 15'' 484")
+imola = Circuit(id='imola', name='Imola', city='Imola', lap_length=4.909, best_lap="1' 15'' 484")
 miami = Circuit(id='miami', name='Miami', city='Miami', lap_length=5.412, best_lap="1' 31'' 361")
 spain = Circuit(id='spain', name='Espagne', city='Barcelona', lap_length=4.675, best_lap="1' 18'' 149")
 monaco = Circuit(id='monaco', name='Monaco', city='Monaco', lap_length=3.337, best_lap="1' 12'' 909")
