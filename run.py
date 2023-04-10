733c2cb6bf7e4e9a7c94f1d5ee977da34fe7992b
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-04-10 19:55:09 +0200

    Make sure current_session is pickled and sent to gsheet

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from threading import Thread
+from src.gsheet.gsheet import GSheet
+from config.config import DEFAULT_SPREADSHEET_ID, RACE_RANKING_RANGE
+import sys
+import pickle
+from datetime import datetime
 from f1_22_telemetry.packets import *
 import logging
 from src.telemetry.telemetry_command import Command
 
-from telemetry import run_telemetry
+from telemetry import TelemetryThread
 from bot import bot
 from config.config import discord_bot_token
 
@@ -24,9 +28,35 @@ logging.basicConfig(
 )
 _logger = logging.getLogger(__name__)
 
-thread_telemetry = Thread(target=lambda: run_telemetry(args.ip, args.sheet_name, bot))
-thread_telemetry.daemon = True
+thread_telemetry = TelemetryThread(args.ip, args.sheet_name, bot)
+thread_telemetry.daemon = True  # this is a problem for pickle and send to gsheet
 thread_telemetry.start()
 
-_logger.info('Starting bot...')
-bot.run(discord_bot_token)
+try:
+    _logger.info('Starting bot...')
+    bot.run(discord_bot_token)
+    _logger.info('Bot stopped, hit CTRL+C again to stop telemetry')
+    thread_telemetry.join()
+except KeyboardInterrupt:
+    _logger.info('Stopping telemetry...')
+    with open(f"session{datetime.now().isoformat()}.pickle", "wb") as out_file:
+        try:
+            pickle.dump(thread_telemetry.brain, out_file)
+        except Exception as e:
+            _logger.error('Could not pickle the brain because of following exception')
+            _logger.error(e)
+            _logger.info('Pickling current session instead...')
+            pickle.dump(thread_telemetry.brain.current_session, out_file)
+        if thread_telemetry.brain.current_session and thread_telemetry.brain.current_session.final_classification:
+            _logger.info('final ranking found, will be sent to google sheet !')
+            final_ranking = thread_telemetry.brain.current_session.get_formatted_final_ranking()
+            for row in final_ranking:
+                print('\t'.join(map(str, row)))
+            if thread_telemetry.sheet_name:
+                g = GSheet()
+                range_str = f"'{thread_telemetry.sheet_name}'!{RACE_RANKING_RANGE}"
+                g.set_sheet_values(DEFAULT_SPREADSHEET_ID, range_str, final_ranking)
+                _logger.info(f'Writing ranking above to sheet {thread_telemetry.sheet_name}')
+        else:
+            _logger.info('No final ranking found, exit')
+    sys.exit(130)
--- telemetry.py
+++ telemetry.py
@@ -1,6 +1,7 @@
 import sys
 import pickle
 from datetime import datetime
+from threading import Thread
 from disnake.ext import commands
 from f1_22_telemetry.listener import TelemetryListener
 from f1_22_telemetry.packets import *
@@ -12,40 +13,46 @@ from config.config import DEFAULT_SPREADSHEET_ID, RACE_RANKING_RANGE
 
 _logger = logging.getLogger(__name__)
 
+class TelemetryThread(Thread):
+    def __init__(self, ip:str, sheet_name:str=None, bot:commands.InteractionBot=None):
+        super().__init__()
+        self.ip = ip
+        self.sheet_name = sheet_name
+        self.bot = bot
+        self.brain = Brain(bot)
 
-def run_telemetry(ip:str, sheet_name:str=None, bot:commands.InteractionBot=None):
-    brain = Brain(bot)
-    _logger.info(f'Starting listening on {ip}:20777')
-    listener = TelemetryListener(host=ip)
-    i = 0
-    try:
-        while True:
-            _logger.debug('Waiting for packets...')
-            packet = listener.get()
-            packet_type = type(packet)
-            _logger.debug(f'{packet_type} received...')
-            brain.handle_received_packet(packet)
-            if brain.current_session and brain.current_session.participants:
-                for participant in brain.current_session.participants:
-                    if not participant.telemetry_is_public:
-                        print(f'{participant.race_number} {participant.name} does not have public telemetry !')
-            i += 1
-    except KeyboardInterrupt:
-        _logger.info('Stopping telemetry...')
-        with open(f"session{datetime.now().isoformat()}.pickle", "wb") as out_file:
-            pickle.dump(brain, out_file)
-            if brain.current_session and brain.current_session.final_classification:
-                final_ranking = brain.current_session.get_formatted_final_ranking()
-                for row in final_ranking:
-                    print('\t'.join(map(str, row)))
-                if sheet_name:
-                    g = GSheet()
-                    range_str = f"'{sheet_name}'!{RACE_RANKING_RANGE}"
-                    g.set_sheet_values(DEFAULT_SPREADSHEET_ID, range_str, final_ranking)
-                    _logger.info(f'Writing ranking above to sheet {sheet_name}')
-        sys.exit(130)
-    except:
-        _logger.info('Stopping telemetry because of huge fail...')
-        with open(f"session{datetime.now().isoformat()}.pickle", "wb") as out_file:
-            pickle.dump(brain, out_file)
-        raise
+    def run(self) -> None:
+        _logger.info(f'Starting listening on {self.ip}:20777')
+        listener = TelemetryListener(host=self.ip)
+        i = 0
+        try:
+            while True:
+                _logger.debug('Waiting for packets...')
+                packet = listener.get()
+                packet_type = type(packet)
+                _logger.debug(f'{packet_type} received...')
+                self.brain.handle_received_packet(packet)
+                if self.brain.current_session and self.brain.current_session.participants:
+                    for participant in self.brain.current_session.participants:
+                        if not participant.telemetry_is_public:
+                            print(f'{participant.race_number} {participant.name} does not have public telemetry !')
+                i += 1
+        except KeyboardInterrupt:
+            _logger.info('Stopping telemetry...')
+            with open(f"session{datetime.now().isoformat()}.pickle", "wb") as out_file:
+                pickle.dump(self.brain, out_file)
+                if self.brain.current_session and self.brain.current_session.final_classification:
+                    final_ranking = self.brain.current_session.get_formatted_final_ranking()
+                    for row in final_ranking:
+                        print('\t'.join(map(str, row)))
+                    if self.sheet_name:
+                        g = GSheet()
+                        range_str = f"'{self.sheet_name}'!{RACE_RANKING_RANGE}"
+                        g.set_sheet_values(DEFAULT_SPREADSHEET_ID, range_str, final_ranking)
+                        _logger.info(f'Writing ranking above to sheet {self.sheet_name}')
+            sys.exit(130)
+        except:
+            _logger.info('Stopping telemetry because of huge fail...')
+            with open(f"session{datetime.now().isoformat()}.pickle", "wb") as out_file:
+                pickle.dump(self.brain, out_file)
+            raise
