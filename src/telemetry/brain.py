0ba405af2b03e1919693d9a37225454c0d325fd1
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-04-24 18:43:00 +0200

    [FIX] print only forecast for current session

--- src/telemetry/brain.py
+++ src/telemetry/brain.py
@@ -86,13 +86,14 @@ class Brain:
                 wfcasts = self.current_session.weather_forecast
                 rows = []
                 for wfcast in wfcasts:
-                    rows.append([
-                        f'+{wfcast.time_offset}min',
-                        str(wfcast.weather),
-                        f'{wfcast.rain_percentage}% pluie',
-                        f'Circuit: {wfcast.track_temperature}°C',
-                        f'Air: {wfcast.air_temperature}°C',
-                    ])
+                    if wfcast.session_type == self.current_session.session_type:
+                        rows.append([
+                            f'+{wfcast.time_offset}min',
+                            str(wfcast.weather),
+                            f'{wfcast.rain_percentage}% pluie',
+                            f'Circuit: {wfcast.track_temperature}°C',
+                            f'Air: {wfcast.air_temperature}°C',
+                        ])
                 msg = tabulate(rows, tablefmt='simple_grid')
                 self._send_discord_message(f"```\n{msg}\n```")
             if 'safety_car_status' in changes:
