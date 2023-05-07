1c55df1ba0c998ec90f8c6d98a2ac2d648a35e2f
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-07 17:19:40 +0200

    FIX

--- src/telemetry/telemetry_command.py
+++ src/telemetry/telemetry_command.py
@@ -7,6 +7,6 @@ class Command(argparse.ArgumentParser):
         self.add_argument("ip", help="Ip address")
         self.add_argument("--log-level", help="Log level", dest='log_level', default='info')
         self.add_argument('--final-sheet', help='Sheet to use to store final classification of last session', dest='sheet_name', default=None)
-        self.add_argument('g','--discord-guild', help='Discord guild id', dest='discord_guild', default=None)
-        self.add_argument('d','--discord-channel', help='Discord channel id', dest='discord_channel', default=None)
+        self.add_argument('-g','--discord-guild', help='Discord guild id', dest='discord_guild', default=None)
+        self.add_argument('-d','--discord-channel', help='Discord channel id', dest='discord_channel', default=None)
 
