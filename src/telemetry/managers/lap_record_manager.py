3afb3cef31121c9506f222c234c3a7d44b5c826c
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-07 23:20:33 +0200

    FIX

--- src/telemetry/managers/lap_record_manager.py
+++ src/telemetry/managers/lap_record_manager.py
@@ -42,16 +42,16 @@ class LapRecordManager(AbstractManager):
 
         if 'best_sector1_lap_num' in changes:
             best_sector1_data = packet.lap_history_data[packet.best_sector1_lap_num-1]
-            changes['best_sector1_time'] = Change(actual=best_sector1_data.lap_time_in_ms, old=lap_record.best_sector1_time)
-            lap_record.best_sector1_time = best_sector1_data.lap_time_in_ms
+            changes['best_sector1_time'] = Change(actual=best_sector1_data.sector1_time_in_ms, old=lap_record.best_sector1_time)
+            lap_record.best_sector1_time = best_sector1_data.sector1_time_in_ms
 
         if 'best_sector2_lap_num' in changes:
             best_sector2_data = packet.lap_history_data[packet.best_sector2_lap_num-1]
-            changes['best_sector2_time'] = Change(actual=best_sector2_data.lap_time_in_ms, old=lap_record.best_sector2_time)
-            lap_record.best_sector2_time = best_sector2_data.lap_time_in_ms
+            changes['best_sector2_time'] = Change(actual=best_sector2_data.sector2_time_in_ms, old=lap_record.best_sector2_time)
+            lap_record.best_sector2_time = best_sector2_data.sector2_time_in_ms
 
         if 'best_sector3_lap_num' in changes:
             best_sector3_data = packet.lap_history_data[packet.best_sector3_lap_num-1]
-            changes['best_sector3_time'] = Change(actual=best_sector3_data.lap_time_in_ms, old=lap_record.best_sector3_time)
-            lap_record.best_sector3_time = best_sector3_data.lap_time_in_ms
+            changes['best_sector3_time'] = Change(actual=best_sector3_data.sector3_time_in_ms, old=lap_record.best_sector3_time)
+            lap_record.best_sector3_time = best_sector3_data.sector3_time_in_ms
         return changes
