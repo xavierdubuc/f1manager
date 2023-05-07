956fcb803984c3921780204f1b2717ad087277c5
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-07 23:11:48 +0200

    IMP to string

--- src/telemetry/models/lap_record.py
+++ src/telemetry/models/lap_record.py
@@ -7,3 +7,11 @@ class LapRecord:
     best_sector1_time: int = None
     best_sector2_time: int = None
     best_sector3_time: int = None
+
+    def __str__(self):
+        return (
+            f"{self.best_lap_time} (Lap {self.best_lap_time_lap_num})\n"
+            f"Sector 1: {self.best_sector1_time} (Lap {self.best_sector1_lap_num})\n"
+            f"Sector 2: {self.best_sector2_time} (Lap {self.best_sector2_lap_num})\n"
+            f"Sector 3: {self.best_sector3_time} (Lap {self.best_sector2_lap_num})"
+        )
