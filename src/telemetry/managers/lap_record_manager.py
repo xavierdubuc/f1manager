e106e8761daecfdb21787fdf26c916d248b76d59
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-07 23:17:20 +0200

    FIX

--- src/telemetry/managers/lap_record_manager.py
+++ src/telemetry/managers/lap_record_manager.py
@@ -19,16 +19,16 @@ class LapRecordManager(AbstractManager):
     @classmethod
     def create(cls, packet: PacketSessionHistoryData) -> LapRecord:
         self = super().create(packet)
-        best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num]
+        best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num-1]
         self.best_lap_time = best_lap_data.lap_time_in_ms
 
-        best_sector1_data = packet.lap_history_data[packet.best_sector1_lap_num]
+        best_sector1_data = packet.lap_history_data[packet.best_sector1_lap_num-1]
         self.best_sector1_time = best_sector1_data.lap_time_in_ms
 
-        best_sector2_data = packet.lap_history_data[packet.best_sector2_lap_num]
+        best_sector2_data = packet.lap_history_data[packet.best_sector2_lap_num-1]
         self.best_sector2_time = best_sector2_data.lap_time_in_ms
 
-        best_sector3_data = packet.lap_history_data[packet.best_sector3_lap_num]
+        best_sector3_data = packet.lap_history_data[packet.best_sector3_lap_num-1]
         self.best_sector3_time = best_sector3_data.lap_time_in_ms
         return self
 
@@ -36,22 +36,22 @@ class LapRecordManager(AbstractManager):
     def update(cls, lap_record, packet: PacketSessionHistoryData) -> Dict[str, Change]:
         changes = super().update(lap_record, packet)
         if 'best_lap_time_lap_num' in changes:
-            best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num]
+            best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num-1]
             changes['best_lap_time'] = Change(actual=best_lap_data.lap_time_in_ms, old=lap_record.best_lap_time)
             lap_record.best_lap_time = best_lap_data.lap_time_in_ms
 
         if 'best_sector1_time_lap_num' in changes:
-            best_sector1_data = packet.lap_history_data[packet.best_sector1_time_lap_num]
+            best_sector1_data = packet.lap_history_data[packet.best_sector1_time_lap_num-1]
             changes['best_sector1_time'] = Change(actual=best_sector1_data.lap_time_in_ms, old=lap_record.best_sector1_time)
             lap_record.best_sector1_time = best_sector1_data.lap_time_in_ms
 
         if 'best_sector2_time_lap_num' in changes:
-            best_sector2_data = packet.lap_history_data[packet.best_sector2_time_lap_num]
+            best_sector2_data = packet.lap_history_data[packet.best_sector2_time_lap_num-1]
             changes['best_sector2_time'] = Change(actual=best_sector2_data.lap_time_in_ms, old=lap_record.best_sector2_time)
             lap_record.best_sector2_time = best_sector2_data.lap_time_in_ms
 
         if 'best_sector3_time_lap_num' in changes:
-            best_sector3_data = packet.lap_history_data[packet.best_sector3_time_lap_num]
+            best_sector3_data = packet.lap_history_data[packet.best_sector3_time_lap_num-1]
             changes['best_sector3_time'] = Change(actual=best_sector3_data.lap_time_in_ms, old=lap_record.best_sector3_time)
             lap_record.best_sector3_time = best_sector3_data.lap_time_in_ms
         return changes
