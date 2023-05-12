80de8d818f2f277bc20be6c854c6978e35775a7f
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-12 10:30:58 +0200

    [FIX]

--- src/telemetry/managers/lap_record_manager.py
+++ src/telemetry/managers/lap_record_manager.py
@@ -19,20 +19,19 @@ class LapRecordManager(AbstractManager):
     @classmethod
     def create(cls, packet: PacketSessionHistoryData) -> LapRecord:
         self = super().create(packet)
-        print(packet.best_lap_time_lap_num)
-        if packet.best_lap_time_lap_num > 1:
+        if 1 <= packet.best_lap_time_lap_num <= len(packet.lap_history_data):
             best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num-1]
             self.best_lap_time = best_lap_data.lap_time_in_ms
 
-        if packet.best_sector1_lap_num > 1:
+        if 1 <= packet.best_sector1_lap_num <= len(packet.best_sector1_lap_num):
             best_sector1_data = packet.lap_history_data[packet.best_sector1_lap_num-1]
             self.best_sector1_time = best_sector1_data.sector1_time_in_ms
 
-        if packet.best_sector2_lap_num > 1:
+        if 1 <= packet.best_sector2_lap_num <= len(packet.best_sector2_lap_num):
             best_sector2_data = packet.lap_history_data[packet.best_sector2_lap_num-1]
             self.best_sector2_time = best_sector2_data.sector2_time_in_ms
 
-        if packet.best_sector3_lap_num > 1:
+        if 1 <= packet.best_sector3_lap_num <= len(packet.best_sector3_lap_num):
             best_sector3_data = packet.lap_history_data[packet.best_sector3_lap_num-1]
             self.best_sector3_time = best_sector3_data.sector3_time_in_ms
         return self
