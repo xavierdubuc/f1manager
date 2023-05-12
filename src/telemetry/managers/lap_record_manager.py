1b23d800c3b6243f8a8c3f89c2695e94c59330ba
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-12 10:33:33 +0200

    FIX

--- src/telemetry/managers/lap_record_manager.py
+++ src/telemetry/managers/lap_record_manager.py
@@ -23,15 +23,15 @@ class LapRecordManager(AbstractManager):
             best_lap_data = packet.lap_history_data[packet.best_lap_time_lap_num-1]
             self.best_lap_time = best_lap_data.lap_time_in_ms
 
-        if 1 <= packet.best_sector1_lap_num <= len(packet.best_sector1_lap_num):
+        if 1 <= packet.best_sector1_lap_num <= len(packet.lap_history_data):
             best_sector1_data = packet.lap_history_data[packet.best_sector1_lap_num-1]
             self.best_sector1_time = best_sector1_data.sector1_time_in_ms
 
-        if 1 <= packet.best_sector2_lap_num <= len(packet.best_sector2_lap_num):
+        if 1 <= packet.best_sector2_lap_num <= len(packet.lap_history_data):
             best_sector2_data = packet.lap_history_data[packet.best_sector2_lap_num-1]
             self.best_sector2_time = best_sector2_data.sector2_time_in_ms
 
-        if 1 <= packet.best_sector3_lap_num <= len(packet.best_sector3_lap_num):
+        if 1 <= packet.best_sector3_lap_num <= len(packet.lap_history_data):
             best_sector3_data = packet.lap_history_data[packet.best_sector3_lap_num-1]
             self.best_sector3_time = best_sector3_data.sector3_time_in_ms
         return self
