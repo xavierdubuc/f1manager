c7c66ec9f3bd0ab2bd116770070a7f41a5ebfd08
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-05-14 01:49:25 +0200

    try

--- src/telemetry/models/weather_forecast.py
+++ src/telemetry/models/weather_forecast.py
@@ -52,7 +52,7 @@ class WeatherForecast:
 
         time_offset = f'+ {str(self.time_offset).rjust(3)}min'.rjust(max_length)
         weather = str(self.weather)
-        rain = f'{rain_percentage_str} `{self.rain_percentage.rjust(3)}%'.rjust(max_length-5)+'`'
+        rain = f'{rain_percentage_str} `{str(self.rain_percentage).rjust(3)}%'.rjust(max_length-5)+'`'
         track = f'Piste: {self.track_temperature}°C'.rjust(max_length)
         air = f'Air: {self.air_temperature}°C'.rjust(max_length),
         return [
