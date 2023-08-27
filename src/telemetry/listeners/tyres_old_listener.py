fa2e8e5a499527b47556d36de943b05caa6eb20b
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-08-27 11:37:40 +0200

    dump2debug

--- src/telemetry/listeners/tyres_old_listener.py
+++ src/telemetry/listeners/tyres_old_listener.py
@@ -23,12 +23,11 @@ class TyresOldListener(AbstractListener):
     def _on_damage_updated(self, damage: Damage, changes: Dict[str, Change], participant: Participant, session: Session) -> List[Message]:
         if 'tyres_damage' in changes:
             tyres_wear = changes.get('tyres_damage').actual
-            print('--')
-            print(participant.name)
-            print(damage)
-            print(changes)
-            print(self.notified)
-            print('--')
+            if participant.name[-3:] == 'REZ':
+                print(participant)
+                print(damage)
+                print(changes)
+                print(self.notified)
             if self.notified.get(participant.name, False): # if he's already notified
                 if all(t < 70 for t in tyres_wear):
                     self.notified[participant.name] = False
