d44777c74c7c6f50cedd7f8253b2d9ddb48d7e78
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-08-27 13:38:24 +0200

    FIX

--- src/telemetry/models/enums/surface_type.py
+++ src/telemetry/models/enums/surface_type.py
@@ -2,15 +2,15 @@ from enum import Enum
 
 
 class SurfaceType(Enum):
-    tarmac: 0
-    rumble_strip: 1  # vibreur
-    concrete: 2
-    rock: 3
-    gravel: 4
-    mud: 5
-    sand: 6
-    grass: 7
-    water: 8
-    cobblestone: 9 # pavé
-    metal: 10
-    ridge: 11
+    tarmac = 0
+    rumble_strip = 1  # vibreur
+    concrete = 2
+    rock = 3
+    gravel = 4
+    mud = 5
+    sand = 6
+    grass = 7
+    water = 8
+    cobblestone = 9 # pavé
+    metal = 10
+    ridge = 11
