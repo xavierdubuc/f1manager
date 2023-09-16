9b1ac07d764dc031a2146efc617cd40856239c40
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-09-16 23:37:48 +0200

    [IMP] team_ranking_generator: make it configurable

--- src/media_generation/generators/teams_ranking_generator.py
+++ src/media_generation/generators/teams_ranking_generator.py
@@ -1,3 +1,4 @@
+import logging
 from PIL import Image
 from ..generators.abstract_generator import AbstractGenerator
 
@@ -6,41 +7,61 @@ from ..font_factory import FontFactory
 from ..models import Team, Visual
 from ..data import teams_idx
 
+_logger = logging.getLogger(__name__)
+
 class TeamsRankingGenerator(AbstractGenerator):
     def _get_visual_type(self) -> str:
-        return 'ranking'
+        return 'teams_ranking'
 
     def _generate_basic_image(self) -> PngImageFile:
-        width = 1080
-        height = 1440
+        width = self.visual_config['width']
+        height = self.visual_config['height']
+        _logger.info(f'Output size is {width}px x {height}px')
+        # FIXME use a BG image ?
         return Image.new('RGB', (width, height), (0,0,0))
 
     def _generate_title_image(self, base_img: PngImageFile) -> PngImageFile:
         height = 300
         img = Image.new('RGB', (base_img.width,height), (255,255,255))
 
-        with Visual.get_fbrt_round_logo('white') as logo:
-            logo = resize(logo, logo.width, img.height-40)
-            logo_pos = paste(logo, img, left=40)
+        # LEFT LOGO
+        left_logo_config = self.visual_config.get('left_logo')
+        if left_logo_config:
+            with Image.open(left_logo_config['path']) as left_logo:
+                left_logo = resize(left_logo, height=left_logo_config['height'])
+            logo_pos = paste(left_logo, img, left=left_logo_config['left'])
 
-        with Visual.get_fif_logo('wide') as logo:
-            logo = resize(logo, height=int(0.4*img.height))
-            paste(logo, img, left=img.width-logo.width, top=height-logo.height)
+        # RIGHT LOGO
+        right_logo_config = self.visual_config.get('right_logo')
+        if right_logo_config:
+            right_padding_top = right_logo_config['padding_top']
+            with Image.open(right_logo_config['path']) as right_logo:
+                right_logo = resize(right_logo, height=right_logo_config['height'])
+            paste(right_logo, img, left=img.width-right_logo.width, top=height-right_logo.height - right_padding_top)
+
+        # Main title
+        title_cfg = self.visual_config['title']
+        font_size = title_cfg.get('font_size', 60)
+        font_color = title_cfg.get('font_color', (0,0,0))
+        font_name = title_cfg.get('font')
+        txt_font = FontFactory.get_font(font_name, font_size, FontFactory.black)
 
         parts = self.config.ranking_title.split(' ')
         title_parts = [' '.join(parts[:2])] + parts[2:]
-
-        txt_left = logo_pos.right + 50
-        txt_top = 30
-        big_txt_font = FontFactory.black(60)
+        txt_left = logo_pos.right + title_cfg['padding_left']
+        txt_top = title_cfg['top']
 
         for title_part in title_parts:
-            big_txt = text(title_part, (0,0,0), big_txt_font)
+            big_txt = text(title_part, font_color, txt_font)
             big_txt_pos = paste(big_txt, img, left=txt_left, top=txt_top)
             txt_top = big_txt_pos.bottom+10
 
-        small_txt_font = FontFactory.regular(30)
-        small_txt = text(self.config.ranking_subtitle, (0,0,0), small_txt_font)
+        # Sub title
+        small_font_size = title_cfg.get('small_font_size', 30)
+        small_font_color = title_cfg.get('small_font_color', (0,0,0))
+        small_font_name = title_cfg.get('small_font')
+        small_txt_font = FontFactory.get_font(small_font_name, small_font_size, FontFactory.regular)
+        small_txt = text(self.config.ranking_subtitle, small_font_color, small_txt_font)
         paste(small_txt, img, left=txt_left, top=big_txt_pos.bottom+20)
 
         return img
@@ -53,8 +74,10 @@ class TeamsRankingGenerator(AbstractGenerator):
         row_height = ((base_img.height - 300 - padding_top) // 10) - padding_between_rows
         # row_height = 87
         current_top = title_height+padding_top
-        for _, row in self.config.ranking.iterrows():
-            team_ranking_img = self._get_team_ranking_img(width, row_height, row['Ecurie'], row['Total'])
+        metric = 'Total'
+        self.config.ranking[metric] = self.config.ranking[metric].str.replace(',','.').astype(int)
+        for _, row in self.config.ranking.sort_values(by=metric, ascending=False).iterrows():
+            team_ranking_img = self._get_team_ranking_img(width, row_height, row['Ecurie'], row[metric])
             pos = paste(team_ranking_img, base_img, top=current_top)
             current_top = pos.bottom + padding_between_rows
 
@@ -73,12 +96,31 @@ class TeamsRankingGenerator(AbstractGenerator):
         return img
 
     def _get_team_img(self, width:int, height: int, team:Team):
-        with Image.open(f'assets/teams/cards/{team.name}.png') as img:
-            return img.copy()
+        card = team.build_card_image(width, height)
+
+        cfg = self.visual_config['rows']['team']
+        font_name = cfg.get('font')
+        font_size = cfg.get('font_size', 54)
+        font_color = team.standing_fg
+        font = FontFactory.get_font(font_name, font_size, FontFactory.black)
+        left = cfg.get('left', 0)
+        top = cfg.get('top', False)
+
+        team_name = text(team.title, font_color, font)
+        paste(team_name, card, left=left, top=top)
+
+        return card
 
     def _get_points_img(self, width:int, height: int, points:str):
         img = Image.new('RGB', (width, height), (255, 255, 255))
-        points_font = FontFactory.black(80)
-        points_txt = text(points, (0,0,0), points_font)
-        paste(points_txt, img, top=2)
+
+        cfg = self.visual_config['rows']['points']
+        font_name = cfg.get('font')
+        font_size = cfg.get('font_size', 70)
+        font_color = cfg['font_color']
+        font = FontFactory.get_font(font_name, font_size, FontFactory.black)
+
+        points_top = cfg.get('top', 0)
+        points_txt = text(str(points), font_color, font)
+        paste(points_txt, img, top=points_top)
         return img
