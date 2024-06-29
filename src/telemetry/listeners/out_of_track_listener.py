fa103de6c41b6749a57ac894af328a48da0a6d26
Xavier Dubuc <xavier.dubuc@haulogy.net>
2024-06-29 16:58:03 +0200

    FIX

--- src/telemetry/listeners/out_of_track_listener.py
+++ src/telemetry/listeners/out_of_track_listener.py
@@ -24,12 +24,19 @@ class OutOfTrackListener(AbstractListener):
 
     def _on_telemetry_updated(self, telemetry:Telemetry, changes: Dict[str, Change], participant: Participant, session: Session) -> List[Message]:
         if 'surface_types' in changes:
+            old_surfaces = changes['surface_types'].old
             surfaces = changes['surface_types'].actual
             teamoji = self.get_emoji(participant.team.as_emoji())
             lap = session.get_current_lap(participant)
             position = str(lap.car_position).rjust(2)
-            if not all(surface.is_on_track() for surface in surfaces):
+            is_on_track = all(surface.is_on_track() for surface in surfaces)
+            was_on_track = all(surface.is_on_track() for surface in old_surfaces)
+            if was_on_track and not is_on_track:
                 msg = f"`{position}` {teamoji} {participant} est sorti de la piste !"
                 _logger.debug(f'{participant} : {surfaces}')
-            return [Message(content=msg, channel=Channel.DEFAULT)]
+                return [Message(content=msg, channel=Channel.DEFAULT)]
+            elif not was_on_track and is_on_track:
+                msg = f"`{position}` {teamoji} {participant} est revenu sur la piste !"
+                _logger.debug(f'{participant} : {surfaces}')
+                return [Message(content=msg, channel=Channel.DEFAULT)]
             
