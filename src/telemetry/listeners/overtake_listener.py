844d631bf020689ed4db3a6fd4df2729d71ea377
Xavier Dubuc <xavier.dubuc@haulogy.net>
2024-06-29 16:14:02 +0200

    Send overtake message only in race condition + add position in message

--- src/telemetry/listeners/overtake_listener.py
+++ src/telemetry/listeners/overtake_listener.py
@@ -14,7 +14,13 @@ class OvertakeListener(AbstractListener):
     ]
 
     def _on_overtake(self, overtaker: Participant, overtaken: Participant, session: Session) -> List[Message]:
+        if not session.session_type.is_race():
+            return None
         teamoji_1 = self.get_emoji(overtaker.team.as_emoji())
         teamoji_2 = self.get_emoji(overtaken.team.as_emoji())
-        msg = f'{teamoji_1} {overtaker} ⏩ {teamoji_2} {overtaken}'
+        overtaker_lap = session.get_current_lap(overtaker)
+        overtaken_lap = session.get_current_lap(overtaken)
+        r_position = str(overtaker_lap.car_position).rjust(2)
+        n_position = str(overtaken_lap.car_position).rjust(2)
+        msg = f'`{r_position}` {teamoji_1} {overtaker} ⏩ `{n_position}` {teamoji_2} {overtaken}'
         return [Message(content=msg, channel=Channel.DEFAULT)]
