b8568740b87063522315cd13e7532cd6f2f2e2f0
Xavier Dubuc <xavier.dubuc@haulogy.net>
2024-07-09 16:07:44 +0200

    Add debug info

--- src/telemetry/listeners/abstract_listener.py
+++ src/telemetry/listeners/abstract_listener.py
@@ -1,8 +1,12 @@
+import logging
 from dataclasses import dataclass, field
 from typing import Dict, List
 
 from src.telemetry.message import Message
+
 from ..event import Event
+
+_logger = logging.getLogger(__name__)
 
 @dataclass
 class AbstractListener:
@@ -12,12 +16,15 @@ class AbstractListener:
 
     def on(self, event:Event, *args, **kwargs) -> List[Message]:
         unsp_name = event.name.lower()
-        method = getattr(self, f'_on_{unsp_name}')
+        method_name = f'_on_{unsp_name}'
+        method = getattr(self, method_name)
         if method:
             res = method(*args, **kwargs)
             if res and not isinstance(res, list):
                 return list(res)
             return res
+        else:
+            _logger.debug(f'{self.__class__.__name__}.{method_name} does not exist !')
 
     def get_emoji(self, emoji_name:str, str_if_not_found:str = None) -> str:
         return (self.emojis or {}).get(emoji_name, str_if_not_found or emoji_name)
