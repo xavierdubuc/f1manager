53fc050018d126b8d775a705b9cd85193f203ee9
Xavier Dubuc <xavier.dubuc@haulogy.net>
2024-07-10 17:19:02 +0200

    better log motion

--- src/telemetry/listeners/spin_listener.py
+++ src/telemetry/listeners/spin_listener.py
@@ -19,7 +19,9 @@ class SpinListener(AbstractListener):
 
     def _on_motion_updated(self, motion: Motion, changes: Dict[str, Change], participant: Participant, session: Session):
         if participant.original_driver == OriginalDriver.nico_hulkenberg:
-            _logger.info(changes)
-            _logger.warning(motion.yaw)
-            _logger.warning(motion.pitch)
-            _logger.warning(motion.roll)
+            _logger.warning(('POSITION :', round(motion.world_position_x, 2), round(motion.world_position_y, 2), round(motion.world_position_z, 2)))
+            _logger.warning(('RIGHT  :', round(motion.world_right_dir_x, 2), round(motion.world_right_dir_y, 2), round(motion.world_right_dir_z, 2)))
+            _logger.warning(('FORWARD  :', round(motion.world_forward_dir_x, 2), round(motion.world_forward_dir_y, 2), round(motion.world_forward_dir_z, 2)))
+            _logger.warning(('VELOCITY  :', round(motion.world_velocity_x, 2), round(motion.world_velocity_y, 2), round(motion.world_velocity_z, 2)))
+            _logger.warning(('G-FORCE  :', round(motion.g_force_lateral, 2), round(motion.g_force_longitudinal, 2), round(motion.g_force_vertical, 2)))
+            _logger.warning(('YAPITCHROLL  :', round(motion.yaw, 2), round(motion.pitch, 2), round(motion.roll, 2)))
