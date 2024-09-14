599983120b00b1fa9db6ea877307fa1ef62ef8fe
Xavier Dubuc <xavier.dubuc@haulogy.net>
2024-09-15 01:34:26 +0200

    Try to remove redundant speed trap messages

--- src/telemetry/models/speed_trap_entry.py
+++ src/telemetry/models/speed_trap_entry.py
@@ -12,20 +12,24 @@ class SpeedTrapEntry:
     fastest_speed_in_session: float
     fastest_participant: Participant
 
+    @property
+    def participant_speed_as_int(self):
+        return int(self.participant_speed)
+
     def __eq__(self, other:"SpeedTrapEntry"):
-        return self.participant_speed == other.participant_speed
+        return self.participant_speed_as_int == other.participant_speed_as_int
 
     def __lt__(self, other:"SpeedTrapEntry"):
-        return self.participant_speed < other.participant_speed
+        return self.participant_speed_as_int < other.participant_speed_as_int
 
     def __gt__(self, other:"SpeedTrapEntry"):
-        return self.participant_speed > other.participant_speed
+        return self.participant_speed_as_int > other.participant_speed_as_int
 
     def __ge__(self, other:"SpeedTrapEntry"):
-        return self.participant_speed >= other.participant_speed
+        return self.participant_speed_as_int >= other.participant_speed_as_int
 
     def __le__(self, other:"SpeedTrapEntry"):
-        return self.participant_speed <= other.participant_speed
+        return self.participant_speed_as_int <= other.participant_speed_as_int
 
     def __str__(self):
-        return f'{self.participant} : {self.participant_speed} KM/H'
+        return f'{self.participant} : {self.participant_speed_as_int} KM/H'
